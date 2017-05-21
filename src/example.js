98a63203588c378af04d4391d7bb81ced38f5ef0
Joseph Kohlmann <joseph.kohlmann@nytimes.com>
2017-05-20 20:52:37 -0400

    WIP. Having serious issues with Flow types for Express and http.Server

--- src/example.js
+++ src/example.js
@@ -1,9 +1,6 @@
 // @flow
-import slackAuth from './index';
+import travisSetup from './index';
 
 // https://slack.com/oauth/authorize?&client_id=2177882080.186798670246&scope=channels:read,chat:write:bot
 
-slackAuth({
-  client_id: '2177882080.186798670246',
-  scope: 'channels:read,chat:write:bot',
-});
+travisSetup();
